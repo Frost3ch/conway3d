dbbf7952062e1152c1b3490a90d8eff30c5606c8
frosty <alp.waterfall@gmail.com>
2025-08-02 21:10:51 +0100

    initial

---
+++ src/main/java/net/frosty/conway3d/Conway3DClient.java
@@ -0,0 +1,10 @@
+package net.frosty.conway3d;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Conway3DClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/frosty/conway3d/Conway3DDataGenerator.java
@@ -0,0 +1,11 @@
+package net.frosty.conway3d;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Conway3DDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
